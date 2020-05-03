45c09d6c6bfd1a2e597f50210755b76abd3f5584
bertanulusoy <bertan_ulusoy@yahoo.com>
2020-05-03 10:30:19 +0300

    cmake of project is created.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
-#include <memory>
-#include <cmath>
+#include <functional>
 
-struct Base
+int main()
 {
-	virtual void do_thing() = 0;
-	virtual ~Base() = default;
-	Base() = default;
-	Base(const Base &) = default;
-	Base(Base &&) = default;
-	auto operator=(const Base &) -> Base & = default;
-	auto operator=(Base &&) -> Base & = default;
-
-	int data{};
-};
-
-struct Derived :Base
-{
-	void do_thing() override {}
-};
-
-auto main() -> int 
-{
- 	//std::unique_ptr<Base> b = std::unique_ptr<Base>(new Derived());
-	Derived d;
-	d.do_thing();
-	//delete b;
+    [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n"; }();
 }
